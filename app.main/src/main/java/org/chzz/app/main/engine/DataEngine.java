2c17095efce6c8161fe74519934c5bca6e98507d
copy <xiaoxinxing12@qq.com>
2016-04-22 00:21:07 +0800

    新加

---
+++ app.main/src/main/java/org/chzz/app/main/engine/DataEngine.java
@@ -0,0 +1,53 @@
+package org.chzz.app.main.engine;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+
+import org.chzz.app.main.AppContext;
+import org.chzz.app.main.R;
+import org.chzz.app.main.model.BannerModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bingoogolapple.bgabanner.BGABanner;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * 作者:copy 邮件:2499551993@qq.com
+ * 创建时间:15/5/26 上午1:03
+ * 描述:
+ */
+public class DataEngine {
+
+    public static View getCustomHeaderView(final Context context) {
+        View headerView = View.inflate(context, R.layout.view_custom_header, null);
+        final BGABanner banner = (BGABanner) headerView.findViewById(R.id.banner);
+        final List<View> views = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            views.add(View.inflate(context, R.layout.view_image, null));
+        }
+        banner.setViews(views);
+        AppContext.getInstance().getEngine().getBannerModel().enqueue(new Callback<BannerModel>() {
+            @Override
+            public void onResponse(Response<BannerModel> response) {
+                BannerModel bannerModel = response.body();
+                for (int i = 0; i < views.size(); i++) {
+                    Glide.with(context).load(bannerModel.imgs.get(i)).placeholder(R.mipmap.holder).error(R.mipmap.holder).dontAnimate().thumbnail(0.1f).into((ImageView) views.get(i));
+                }
+                banner.setTips(bannerModel.tips);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+            }
+        });
+        return headerView;
+    }
+
+}
---
+++ app.main/src/main/java/org/chzz/app/main/model/StaggeredModel.java
@@ -0,0 +1,11 @@
+package org.chzz.app.main.model;
+
+/**
+ * 作者:copy 邮件:2499551993@qq.com
+ * 创建时间:15/7/9 16:12
+ * 描述:
+ */
+public class StaggeredModel {
+    public String icon;
+    public String desc;
+}
